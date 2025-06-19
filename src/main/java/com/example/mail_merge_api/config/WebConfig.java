1300f22dbae2bc6d96e167272a361b8fe26775fd
viduthranaweera2001 <it21260056@sliit.lk>
2025-06-19 16:19:04 +0530

    add web

--- src/main/java/com/example/mail_merge_api/config/WebConfig.java
+++ src/main/java/com/example/mail_merge_api/config/WebConfig.java
@@ -4,10 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+//    }
+//}
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST");
+        registry.addMapping("/**")
+                .allowedOrigins("https://eloquent-sprite-0a9f3d.netlify.app/")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*");
     }
 }
